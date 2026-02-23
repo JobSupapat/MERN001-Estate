2263ab487ba882fbd369b5c370b2bc9193f2bd2b
jobSupapat <adsupapat@gmail.com>
2026-02-23 19:01:17 +0700

    5th Connect to MongoDB Atlas

--- api/index.js
+++ api/index.js
@@ -1,9 +1,27 @@
+import dns from 'node:dns/promises'
+dns.setServers(['1.1.1.1', '8.8.8.8'])
+
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB Connected !!!'))
+    .catch((err) => console.error("MongoDB Error !!!", err))
+
 
 //เรียกใช้ EXPRESS
 const app = express()
 
+
+//MIDDLEWARE
+app.use(express.json())
+
+
 //RUN EXPRESS SERVER
-app.listen(3000, () => {
-    console.log("Express Server is running on port : 3000 !!!")
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Express Server is running on port : ${port}`)
 })
